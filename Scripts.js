b8e0e70982617b9d93791b5cdcb4c68c814da761
Stian Grønås <42640705+hvl578007@users.noreply.github.com>
2018-09-13 10:51:26 +0200

    "Final version" - innlevering 1
    Innlevering 1 WEB/HTML

---
+++ Scripts.js
@@ -0,0 +1,45 @@
+﻿var hidden_d = false;
+var hidden_f = false;
+var buttonTxt;
+
+function darkMode(id, id2) {
+    buttonTxt = document.getElementById(id).innerHTML;
+    hidden = false;
+    if (buttonTxt == "Darkmode") {
+        buttonTxt = "Normal";
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+        document.getElementById(id).innerHTML = buttonTxt;
+    } else {
+        buttonTxt = "Darkmode";
+        document.body.style.backgroundColor = 'rgb(101, 195, 253)';
+        document.body.style.color = 'black';
+        document.getElementById(id).innerHTML = buttonTxt;
+    }
+    hidden_f = !hidden_f;
+    if (hidden_f) {
+        document.getElementById(id2).style.visibility = 'hidden';
+    } else {
+        document.getElementById(id2).style.visibility = 'visible';
+    }
+}
+function funTimes(id, id2) {
+    buttonTxt = document.getElementById(id).innerHTML;
+    if (buttonTxt == "Fun times") {
+        buttonTxt = "Normal";
+        document.body.style.backgroundColor = 'pink';
+        document.body.style.fontFamily = 'Comic Sans MS, cursive, sans-serif';
+        document.getElementById(id).innerHTML = buttonTxt;
+    } else {
+        buttonTxt = "Fun times";
+        document.body.style.backgroundColor = 'rgb(101, 195, 253)';
+        document.body.style.fontFamily = 'Verdana, Geneva, sans-serif';
+        document.getElementById(id).innerHTML = buttonTxt;
+    }
+    hidden_d = !hidden_d;
+    if (hidden_d) {
+        document.getElementById(id2).style.visibility = 'hidden';
+    } else {
+        document.getElementById(id2).style.visibility = 'visible';
+    }
+}
